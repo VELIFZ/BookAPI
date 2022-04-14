31c7847badb562581029ea70182ab759998e9729
VELIFZ <eliferik72@gmail.com>
2022-04-14 17:37:16 -0400

    added single page

--- app/blueprints/shop/routes.py
+++ app/blueprints/shop/routes.py
@@ -1,17 +1,33 @@
 # Making api call to sell api that is created wirh sell form. 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for
 import requests as r
+from app.models import db, User, Book
 
 shop = Blueprint('shop', __name__, template_folder='shop_templates', url_prefix = '/shop') 
 
+# @shop.route('/buy')
+# def buy():
+#     res = r.get('https://the-book-cafe.herokuapp.com/api/books').json()
+#     if res:
+#         try:
+#             book = res["Books"]
+#             return render_template('buy.html', book=book)    
+#         except:
+#             return ''
+#     return render_template('buy.html')
+
 @shop.route('/buy')
 def buy():
-    res = r.get('https://the-book-cafe.herokuapp.com/api/books').json()
-    if res:
-        try:
-            book = res["Books"]
-            return render_template('buy.html', book=book)    
-        except:
-            return ''
-    return render_template('buy.html')
+    #page = request.args.get('page', 1, type=int)
+    all_books = Book.query.order_by(Book.date_posted.desc()).all()
+    return render_template('buy.html', all_books=all_books)
 
+
+@shop.route('/<string:id>')
+def single_page(id):
+    single_book = Book.query.get(id)
+    return render_template('single_page.html', single_book=single_book)
+
+@shop.route('/cart')
+def addcart():
+    return render_template('cart.html')
